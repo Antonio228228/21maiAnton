3444409b5b447168d81a57ecf1bb3f8bc3f6bf60
opilane <opilane@HP-CZC6297B89>
2021-05-21 12:13:19 +0300

    1

---
+++ Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21maiAnton
+{
+    class Point //class eto tip dannqh
+    {
+        public int x; //peremennqe kotorie zadajut koordinatq to4ki po osi x
+        public int y;
+        public char sym;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,34 @@ namespace _21maiAnton
     {
         static void Main(string[] args)
         {
-            ConsoleWriteLine()
+            Point p1 = new Point(); //ekzemplar classa point
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            Draw(p1.x, p1.y, p1.sym); //vqvodim na ekran
+            /*p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            Draw(p1.x, p1.y, p1.sym);*/
+
+            Point p2 = new Point(); //vtoroi ekzemplar classa point
+            /*int x1 = 1; //int hranit celie 4isla
+            int y1 = 3;
+            char sym1 = '*';  //char hranit symbols
+            Draw(x1, y1, sym1);*/
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            Draw(x2, y2, sym2);
+
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym) 
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
